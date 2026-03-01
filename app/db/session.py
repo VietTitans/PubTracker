a4f4191daa108d32090ae1103df13050b6f58099
MattND <43970143+VietTitans@users.noreply.github.com>
2026-03-01 17:54:06 +0100

    Fix load env

--- app/db/session.py
+++ app/db/session.py
@@ -1,9 +1,13 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.models.base import Base
 
+load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE_URL:", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 
