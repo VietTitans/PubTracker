1727763575004976f744959407606c3ad742e4f8
MattND <43970143+VietTitans@users.noreply.github.com>
2026-06-28 00:49:36 +0200

    Setup DOTNET API

---
+++ RESTAPI/RecordService/Program.cs
@@ -0,0 +1,32 @@
+using DotNetEnv;
+
+Env.Load(Path.Combine(Directory.GetCurrentDirectory(), "../../.env"));
+Console.WriteLine("Hello");
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
